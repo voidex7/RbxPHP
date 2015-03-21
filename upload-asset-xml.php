f883d68f15fd12eeb08655253fefb0a8ecda7c58
Voiliax <oxluaxo@hotmail.com>
2015-03-21 08:31:43 +0400

    updated upload-asset-xml
    made the code neater

--- upload-asset-xml.php
+++ upload-asset-xml.php
@@ -1,18 +1,19 @@
 <?php
 
-// Input
-$id = $_GET['id'];
-$body = file_get_contents('php://input');
-$xml = (ord(substr($body,0,1)) == 31 ? gzinflate(substr($body,10,-8)) : $body); // if gzipped, decode
-
 // Login User Data
-$login_user = 'username=&password=';
-$roblosecurity = file_get_contents('access.txt');
+$login_user       = 'username=&password=';
+$file_path_rs     = 'rs.txt';
+$current_rs       = file_get_contents($file_path_rs);
+
+// Input
+$asset_id    = $_GET['id'];
+$post_body  = file_get_contents('php://input');
+$asset_xml   = (ord(substr($post_body,0,1)) == 31 ? gzinflate(substr($post_body,10,-8)) : $post_body); // if gzipped, decode
 
 // [Function] Update ROBLOSECRUITY
-function updateROBLOSECRUITY()
+function updateRS()
 {
-	global $login_user;
+	global $login_user, file_path_rs;
 
 	$get_cookies = curl_init('https://www.roblox.com/newlogin');
 	curl_setopt_array($get_cookies,
@@ -24,39 +25,41 @@ function updateROBLOSECRUITY()
 		)
 	);
 
-	$roblosecurity = (preg_match('/(\.ROBLOSECURITY=.*?);/', curl_exec($get_cookies), $matches) ? $matches[1] : '');
-	file_put_contents('access.txt', $roblosecurity, true);
+	$rs = (preg_match('/(\.ROBLOSECURITY=.*?);/', curl_exec($get_cookies), $matches) ? $matches[1] : '');
+	file_put_contents(file_path_rs, $rs, true);
 	curl_close($get_cookies);
 
-	return $roblosecurity;
+	return $rs;
 }
 
-// [Function] Upload XML
-function uploadXML($roblosecurity) 
+// [Function] Upload Asset XML
+function uploadAssetXML($rs) 
 {
-	global $id, $xml;
+	global $asset_id, $asset_xml;
 	
-	$upload_xml = curl_init("http://www.roblox.com/Data/Upload.ashx?assetid=$id");
+	$upload_xml = curl_init("http://www.roblox.com/Data/Upload.ashx?assetid=$asset_id");
 	curl_setopt_array($upload_xml,
 		array(
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_POST => true,
-			CURLOPT_HTTPHEADER => array('User-Agent: Roblox/WinINet', "Cookie: $roblosecurity"),
-			CURLOPT_POSTFIELDS => $xml
+			CURLOPT_HTTPHEADER => array('User-Agent: Roblox/WinINet', "Cookie: $rs"),
+			CURLOPT_POSTFIELDS => $asset_xml
 		)
 	);
 
 	$avid = curl_exec($upload_xml);
+
+	if (preg_match('/RobloxDefaultErrorPage/', $avid)) {
+		// RS invalid
+		$avid = uploadAssetXML(updateRS());
+	}
+
 	curl_close($upload_xml);
 
 	return $avid;
 }
 
-// Upload XML
-$avid = uploadXML($roblosecurity);
-if (preg_match('/RobloxDefaultErrorPage/', $avid)) { // update cookie if errored
-	$avid = uploadXML(updateROBLOSECRUITY());
-}
 
-// Echo AVID
+// Upload asset XML and echo AVID
+$avid = uploadAssetXML($current_rs);
 echo (is_numeric($avid) ? $avid : 0);
